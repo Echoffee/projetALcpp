c6e277038d3b57ba540a272edcb93768e61f2eae
Apodeus <ordonez.romain@gmail.com>
2018-04-21 12:40:41 +0200

    Add Command pattern and Add Translate Command

---
+++ projetALcpp/src/Command/CommandTranslate.cpp
@@ -0,0 +1,17 @@
+#include <Command/CommandTranslate.hpp>
+#include <Visitor/VisitorTranslate.hpp>
+
+CommandTranslate::CommandTranslate(float x, float y, IShape* shape) {
+	this->shape = shape;
+	direction = new Vector2(x, y);
+}
+
+void CommandTranslate::execute() {
+	savedState = shape->createMemento();
+	VisitorTranslate* visitor = new VisitorTranslate(direction);
+	shape->accept(visitor);
+}
+
+void CommandTranslate::unexecute() {
+	shape->setMemento(savedState);
+}
